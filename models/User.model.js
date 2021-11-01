bbd0b003c6f39068b97d74f735245eebe8f433c1
GodfreySam <godfreysam09@gmail.com>
2021-11-01 14:37:03 +0100

    preparing for heroku host

---
+++ models/User.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose;
+
+let userSchema = new Schema(
+	{
+		firstname: String,
+		lastname: String,
+		username: String,
+		email: String,
+		password: String,
+		secretToken: String,
+		verified: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true },
+);
+
+module.exports = {
+	User: model("user", userSchema),
+};
