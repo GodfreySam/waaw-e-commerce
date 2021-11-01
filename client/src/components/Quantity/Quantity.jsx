10b37f75b3f2efeadc59fea0fe399dbf463ed7df
GodfreySam <godfreysam09@gmail.com>
2021-11-01 14:06:05 +0100

    took all frontend to a client directory for hosting purposes

---
+++ client/src/components/Quantity/Quantity.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+    // color: 'black'
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    color: 'black'
+
+  },
+}));
+
+const Quantity = () =>{
+  const classes = useStyles();
+  const [quantity, setQuantity] = useState('');
+
+  const handleChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Quantity</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          value={quantity}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+          </MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default Quantity
---
+++ client/src/components/pageSix/Pagesix.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./pageSix.css";
+
+const Pagesix = () => {
+
+	return (
+		<div className="pageSix home">
+			<div className="pageSix-content">
+				<div className="pageSix-content__left">
+					<img
+						className="pageSix-content__left-image"
+						src="https://res.cloudinary.com/freyman/image/upload/v1635417547/grp1-ecom/images/asset1_adcckf.png"
+						alt="palm plant"
+					/>
+				</div>
+				<div className="pageSix-content__right">
+					<div className="pageSix-content__right-header">Get Our Recipes Now!</div>
+					<p className="pageSix-content__right-p">
+						Fruits are an excellent source of vitamins and minerals
+					</p>
+					<form className="pageSix-content__right-form" action="">
+						<input type="email" name="email" placeholder="Email" />
+						<button type="submit">Subscribe</button>
+					</form>
+
+					<div className="pageSix-content__app-market">
+						<button
+							onClick={
+								()=>(window.location.href = "https://itunes.apple.com/")
+							}
+							className="pageSix-content__app-market-button"
+						>
+							<img
+								className="pageSix-content__app-market-button"
+								src="https://res.cloudinary.com/freyman/image/upload/v1635417545/grp1-ecom/images/apple-store_gawirt.png"
+								alt="ios store"
+							/>
+						</button>
+						<button
+							onClick={()=>(window.location.href = "https://play.google.com/store/apps/")}
+							className="pageSix-content__app-market-button"
+						>
+							<img
+								className="pageSix-content__app-market-button"
+								src="https://res.cloudinary.com/freyman/image/upload/v1635417549/grp1-ecom/images/google-store_r3mt4l.png"
+								alt="google play store"
+							/>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Pagesix;
